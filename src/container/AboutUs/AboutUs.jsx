ff8f6a0e4c11462b007fa23c74caadef4c024ca3
Tejaskumar Sureshbhai Patel <tejas1996p@gmail.com>
2023-03-26 16:04:42 +0100

    init

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.T} alt="T_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Tejas has been shucking oysters and turning tables from its home in the heart of Covent Garden since Victorian times. </p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">In 1996, the prime minister, gave a local stallholder by the name of Tejas Patel permission to serve Food in London Street.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans"> Being a Chef never seems like a job, It becomes a True Passion. </p>
+        </div>
+        <p className="p__opensans"> Put your HEAD DOWN & WORK HARD. Never wait for THINGS TO HAPPEN, make them HAPPEN for yourself through HARD GRAFT and NOT GIVING UP. </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Tejas Patel</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
